9f5e10cb45f6d031788547c2dea18f13edcf8d30
WooheeKim <kimdngml12@gmail.com>
2023-03-27 23:25:59 +0900

    1

--- src/main/java/com/team/cwl/interceptor/AdminInterceptor.java
+++ src/main/java/com/team/cwl/interceptor/AdminInterceptor.java
@@ -4,29 +4,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.team.cwl.member.MemberDTO;
+import com.team.cwl.members.MemberDTOS;
 
-@Component
-public abstract class CartInterceptor implements HandlerInterceptor {
+public class AdminInterceptor implements HandlerInterceptor {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
+		
 		HttpSession session = request.getSession();
 		
-		MemberDTO memberDTO = (MemberDTO)session.getAttribute("members");
+		MemberDTOS memberDtos = (MemberDTOS)session.getAttribute("members");
 		
-		if(memberDTO == null) {
-			response.sendRedirect("/");
+		if(memberDtos == null || memberDtos.getAdminCheck() == 0) { // 관리자 계정이 아닌 경우
+			
+			response.sendRedirect("/"); // 홈으로 리다이렉트
+			
 			return false;
-		} else {
-			return true;
-		}		
+		}
 		
+		
+		return true;  //  관리자 계정 로그인 경우 (memberDTOs != null && memberDTOs.getAdmincheck() == 1)
 	}
 	
 }
--- src/main/java/com/team/cwl/members/MemberControllers.java
+++ src/main/java/com/team/cwl/members/MemberControllers.java
@@ -5,8 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
-import org.slf4j.spi.LoggerFactoryBinder;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +15,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping(value = "/member/**")
+@RequestMapping(value = "/members/**")
 public class MemberControllers {
 
-	private static final Logger logger = LoggerFactoryBinder.getLogger(MemberControllers.class);
+	private static final Logger logger = LoggerFactory.getLogger(MemberControllers.class);
 	
 	@Autowired
 	private MemberServices memberServices;
@@ -73,7 +74,7 @@ public class MemberControllers {
 	
 	/* 로그인 */
 	@RequestMapping(value = "login.do", method = RequestMethod.POST)
-	public String loginPost(HttpServletRequest request, MemberDTOS member, RedirectAttributes rttr) throws Exception {
+	public String loginPOST(HttpServletRequest request, MemberDTOS member, RedirectAttributes rttr) throws Exception {
 		
 		HttpSession session = request.getSession();
 		String rawPw = "";
@@ -85,16 +86,19 @@ public class MemberControllers {
 			rawPw = member.getMemberPw(); // 사용자가 제출한 비밀번호
 			encodePw = membercheck.getMemberPw(); // DB에 저장한 인코딩된 비밀번호
 			
-			if(true == pwEncoder.matches(rawPw, encodePw)) { // 비밀번호 일치 여부
+			if(true == pwEncoder.matches(rawPw, encodePw)) { // 비밀번호 일치여부 판단
 				
 				membercheck.setMemberPw(""); // 인코딩된 비밀번호 정보 지움
 				session.setAttribute("member", membercheck); // session에 사용자의 정보 저장
 				return "redirect:/"; // 메인페이지로 이동
+			} else {
+				rttr.addFlashAttribute("result", 0);
+				return "redirect:/member/memberLogin"; // 로그인 페이지로 이동
 			}
-		} else {
+		} else {									// 일치하는 아이디가 존재하지 않을 시(로그인 실패)
 			rttr.addFlashAttribute("result", 0);
 			return "redirect:/member/memberLogin"; // 로그인 페이지로 이동
-		}
+		} 
 		
 	}
 	
