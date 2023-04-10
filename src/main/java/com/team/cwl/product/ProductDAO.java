6b7b953c7d0fc45d9bd879dde769307c078e56e7
myeonghyejin <myeonghyejin@icloud.com>
2023-04-10 17:33:47 +0900

    23_0410

--- src/main/java/com/team/cwl/product/ProductDAO.java
+++ src/main/java/com/team/cwl/product/ProductDAO.java
@@ -6,56 +6,65 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.team.cwl.lesson.LessonImgDTO;
 import com.team.cwl.util.Pagination;
 
 @Repository
 public class ProductDAO {
-	
-	@Autowired
-	private SqlSession sqlSession;
-	private final String NAMESPACE = "com.team.cwl.product.ProductDAO.";
+   
+   @Autowired
+   private SqlSession sqlSession;
+   private final String NAMESPACE = "com.team.cwl.product.ProductDAO.";
 
 //--------------------------------------------
-	
-	/** SELECT **/
-	public List<ProductDTO> getProductList(Pagination pagination) throws Exception {
-		return sqlSession.selectList(NAMESPACE+"getProductList", pagination);
-	}
+   
+   /** SELECT **/
+   public List<ProductDTO> getProductList(Pagination pagination) throws Exception {
+      return sqlSession.selectList(NAMESPACE+"getProductList", pagination);
+   }
 
-	public ProductDTO getProductDetail(ProductDTO productDTO) throws Exception {
-		return sqlSession.selectOne(NAMESPACE+"getProductDetail", productDTO);
-	}
-	
-	public List<ProductImgDTO> getProductImgList(ProductDTO productDTO) throws Exception {
-		return sqlSession.selectList(NAMESPACE+"getProductImgList", productDTO);
-	}
-	
-	public Long getTotalCount(Pagination pagination) throws Exception {
-		return sqlSession.selectOne(NAMESPACE+"getTotalCount", pagination);
-	}
-	
-	/** INSERT **/
-	public int setProductAdd(ProductDTO productDTO) throws Exception {
-		return sqlSession.insert(NAMESPACE+"setProductAdd", productDTO);
-	}
-	
-	public int setProductImgAdd(ProductImgDTO productImgDTO) throws Exception {
-		return sqlSession.insert(NAMESPACE+"setProductImgAdd", productImgDTO);
-	}
+   public ProductDTO getProductDetail(ProductDTO productDTO) throws Exception {
+      return sqlSession.selectOne(NAMESPACE+"getProductDetail", productDTO);
+   }
+   
+   public List<ProductImgDTO> getProductImgList(ProductDTO productDTO) throws Exception {
+      return sqlSession.selectList(NAMESPACE+"getProductImgList", productDTO);
+   }
+   
+   public Long getTotalCount(Pagination pagination) throws Exception {
+      return sqlSession.selectOne(NAMESPACE+"getTotalCount", pagination);
+   }
+   
+   /** INSERT **/
+   public int setProductAdd(ProductDTO productDTO) throws Exception {
+      return sqlSession.insert(NAMESPACE+"setProductAdd", productDTO);
+   }
+   
+   public int setProductImgAdd(ProductImgDTO productImgDTO) throws Exception {
+      return sqlSession.insert(NAMESPACE+"setProductImgAdd", productImgDTO);
+   }
 
-	/** UPDATE **/
-	public int setProductUpdate(ProductDTO productDTO) throws Exception {
-		return sqlSession.update(NAMESPACE+"setProductUpdate", productDTO);
-	}
+   /** UPDATE **/
+   public int setProductUpdate(ProductDTO productDTO) throws Exception {
+      return sqlSession.update(NAMESPACE+"setProductUpdate", productDTO);
+   }
 
-	/** DELETE **/
-	public int setProductDelete(ProductDTO productDTO) throws Exception {
-		return sqlSession.delete(NAMESPACE+"setProductDelete", productDTO);
-	}
-	
-	public int setProductImgDelete(Long imgNum) throws Exception {
-		return sqlSession.delete(NAMESPACE+"setProductImgDelete", imgNum);
-	}
+   /** DELETE **/
+   public int setProductDelete(ProductDTO productDTO) throws Exception {
+      return sqlSession.delete(NAMESPACE+"setProductDelete", productDTO);
+   }
+   
+   public int setProductImgDelete(Long imgNum) throws Exception {
+      return sqlSession.delete(NAMESPACE+"setProductImgDelete", imgNum);
+   }
+
+   public List<ProductImgDTO> getProductImgList(Long productNum) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public ProductDTO getProductDetail(Long productNum) {
+      // TODO Auto-generated method stub
+      return null;
+   }
 
 }
